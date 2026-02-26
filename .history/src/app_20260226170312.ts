4f0f55808ce214a5d06005ea793e6f17dd80a9bc
Sha Fin <99030043+shshafin@users.noreply.github.com>
2026-02-26 17:03:53 +0600

    changes

---
+++ .history/src/app_20260226170312.ts
@@ -0,0 +1,46 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+import path from 'path';
+import './app/cron/nodeCron';
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      'http://178.16.138.218',
+      'http://178.16.138.218:5001',
+      'https://krishokarea.com',
+      'https://www.krishokarea.com',
+    ],
+    credentials: true,
+  }),
+);
+
+const uploadsPath = path.resolve('uploads');
+
+app.use('/uploads', express.static(uploadsPath));
+// routes
+app.use('/api/v1', router);
+
+// home route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/v1', (req, res) => {
+  res.send('API is running...');
+});
+
+// global error handler
+app.use(globalErrorHandler);
+
+// not found route
+app.use(notFound);
+
+export default app;
